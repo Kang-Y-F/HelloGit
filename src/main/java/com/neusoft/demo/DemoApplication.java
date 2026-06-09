db5317e810bb98978090771395f07c66d4b00a12
yuyq9 <717781899@qq.com>
2026-06-09 11:34:36 +0800

    Update DemoApplication.java

--- src/main/java/com/neusoft/demo/DemoApplication.java
+++ src/main/java/com/neusoft/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.neusoft.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//这是一个变化
 @SpringBootApplication
 @MapperScan("com.neusoft.demo.dao")
 public class DemoApplication {
