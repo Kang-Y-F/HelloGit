4b040c1f938c300e5edbdad5d0ab99c9966a12c4
Kang-Y-F <3605374098@qq.com>
2026-06-09 10:48:55 +0800

    init_AIBaseDemo

---
+++ src/test/java/com/neusoft/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.neusoft.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+//Java单元测试框架
+@SpringBootTest
+class DemoApplicationTests {	
+
+
+}
